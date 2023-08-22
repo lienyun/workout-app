64dee8f57b2fb234d63c582c439beb4dcbb6c286
wendy <lienyun.chang@gmail.com>
2023-08-22 16:35:50 +0800

    filterd video, random select video

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <v-app class="ma-5">
     <v-main>
       <h1 class="text-center mb-5">今天健什麼？</h1>
-      <SearchCard />
+      <SearchCard @video="video"/>
       <v-card class="mt-5">
         <h1 class="text-center mb-5 mt-5">就決定做這支了！</h1>
         <div class="ma-5 youtube">
-          <iframe src="https://www.youtube.com/embed/XbygvVcf-s0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <h2>{{ resultVideo.title }}</h2>
+          <iframe :src="resultVideo.embeddedUrl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
         </div>
       </v-card>
     </v-main>
@@ -16,9 +18,12 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import SearchCard from "./components/SearchCard.vue";
-import axios from "axios";
 
+const resultVideo = ref({})
 
+const video = (val)=>{
+  resultVideo.value = val
+}
 // const variable = import.meta.env.VITE_KEY
 // console.log(variable)
 
--- src/components/SearchCard.vue
+++ src/components/SearchCard.vue
@@ -1,82 +1,99 @@
 <template lang="">
-<v-card class="pa-5">
-        <v-row>
-          <v-col cols="12" md="3">
-            <v-select
-              label="訓練部位"
-              variant="outlined"
-              density="compact"
-              hide-details
-              multiple
-              chips
-              v-model="filter.bodyPart"
-              :items="['上肢','臀腿','核心','全身']"
-            >
-            </v-select>
-          </v-col>
-          <v-col cols="12" md="3">
-            <v-checkbox label="跳躍" hide-details v-model="filter.noJump"></v-checkbox>
-          </v-col>
-          <v-col cols="12" md="3">
-            <v-checkbox label="需要器材" hide-details v-model="filter.noEquipment"></v-checkbox>
-          </v-col>
+  <v-card class="pa-5">
+    <v-row>
+      <v-col cols="12" md="3">
+        <v-select
+          label="訓練部位"
+          variant="outlined"
+          density="compact"
+          hide-details
+          multiple
+          chips
+          v-model="filter.bodyPart"
+          :items="['上肢', '臀腿', '核心', '全身']"
+        >
+        </v-select>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-checkbox
+          label="無跳躍"
+          hide-details
+          v-model="filter.noJump"
+        ></v-checkbox>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-checkbox
+          label="無器材"
+          hide-details
+          v-model="filter.noEquipment"
+        ></v-checkbox>
+      </v-col>
 
-          <v-col cols="12" md="3">
-            <v-select
-              label="難易度"
-              variant="outlined"
-              density="compact"
-              hide-details
-              :items="['簡單','適中','困難']"
-              v-model="filter.difficuity"
-            >
-            </v-select>
-          </v-col>
-          <v-col cols="12" md="3">
-            <v-select
-              label="鍛鍊類型"
-              variant="outlined"
-              density="compact"
-              hide-details
-              :items="['有氧','肌力','混合']"
-              v-model="filter.type"
-            >
-            </v-select>
-          </v-col>
-          <v-col cols="12" md="3">
-            <v-select
-              label="時間長度"
-              variant="outlined"
-              density="compact"
-              hide-details
-              :items="['5分鐘內','5-10分鐘','10-20分鐘','20-30分鐘','30分鐘以上']"
-              v-model="filter.time"
-            >
-            </v-select>
-          </v-col>
-          <v-col cols="12" md="3">
-            <v-select
-              label="作者"
-              variant="outlined"
-              density="compact"
-              hide-details
-              :items="['May Fit','Coffee','Emi Wong','Pamela']"
-              v-model="filter.author"
-            >
-            </v-select>
-          </v-col>
-          <v-col cols="12" md="3" class="text-center">
-            <v-btn @click="search">搜尋</v-btn>
-            <v-btn class="ml-5">清除</v-btn>
-          </v-col>
-        </v-row>
-      </v-card>
+      <v-col cols="12" md="3">
+        <v-select
+          label="難易度"
+          variant="outlined"
+          density="compact"
+          hide-details
+          :items="['簡單', '適中', '困難']"
+          v-model="filter.difficuity"
+        >
+        </v-select>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-select
+          label="鍛鍊類型"
+          variant="outlined"
+          density="compact"
+          hide-details
+          :items="['有氧', '肌力', '混合']"
+          v-model="filter.type"
+        >
+        </v-select>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-select
+          label="時間長度"
+          variant="outlined"
+          density="compact"
+          hide-details
+          :items="[
+            '5分鐘內',
+            '5-10分鐘',
+            '10-20分鐘',
+            '20-30分鐘',
+            '30分鐘以上',
+          ]"
+          v-model="filter.time"
+        >
+        </v-select>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-select
+          label="作者"
+          variant="outlined"
+          density="compact"
+          hide-details
+          :items="['May Fit', 'Coffee', 'Emi Wong', 'Pamela']"
+          v-model="filter.author"
+        >
+        </v-select>
+      </v-col>
+      <v-col cols="12" md="3" class="text-center">
+        <v-btn @click="search">搜尋</v-btn>
+        <v-btn class="ml-5">清除</v-btn>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
 <script setup>
 import { ref, onMounted, reactive } from "vue";
 import axios from "axios";
 
-const data = ref([]);
+const allData = ref([]);
+const randomVideo = ref({});
+
+const emit = defineEmits(["searchVideo"]); 
 
 const filter = reactive({
   title: null,
@@ -88,22 +105,22 @@ const filter = reactive({
   equimentType: null,
   time: null,
   difficuity: null,
-  type: null
-})
+  type: null,
+});
 
 // const variable = import.meta.env.VITE_KEY
 // console.log(variable)
 
-onMounted(()=>{
-  getData()
-})
+onMounted(() => {
+  getData();
+});
 const url = `https://sheets.googleapis.com/v4/spreadsheets/${GOOGLE_SPREADSHEET_ID}/values/A1%3AJ39?key=${GOOGLE_APIKey}`
 const getData = async () => {
   const res = await axios.get(url);
-  const key = res.data.values[0]
+  const key = res.data.values[0];
 
-  res.data.values.shift()
-  let orgData = res.data.values
+  res.data.values.shift();
+  let orgData = res.data.values;
 
   let result = [];
 
@@ -114,12 +131,64 @@ const getData = async () => {
     }
     result.push(obj);
   }
-  data.value = result
-  console.log(result)
+
+  let newData = result.map((item) => {
+    item.bodyPart = item.bodyPart.split(", ").map((part) => part.trim());
+
+    if (item.noEquipment === "TRUE") {
+      item.noEquipment = true;
+    } else if (item.noEquipment === "FALSE") {
+      item.noEquipment = false;
+    }
+    if (item.noJump === "TRUE") {
+      item.noJump = true;
+    } else if (item.noJump === "FALSE") {
+      item.noJump = false;
+    }
+
+    let urlId = item.url.split('/').pop()
+    let embeddedUrl = `https://www.youtube.com/embed/${urlId}`
+
+    return {...item, embeddedUrl};
+  });
+  allData.value = newData;
+  console.log("allData", allData.value);
 };
 
+const search = () => {
+  console.log(filter);
+  let filterdData = allData.value.filter((item) => {
+    if (filter.author !== null && item.author !== filter.author) {
+      return false;
+    }
+    if (filter.bodyPart !== null) {
+      if (!item.bodyPart.includes(...filter.bodyPart)) {
+        return false;
+      }
+    }
 
+    if (
+      filter.noEquipment !== null &&
+      item.noEquipment !== filter.noEquipment
+    ) {
+      return false;
+    }
+    if (filter.difficuity !== null && item.difficuity !== filter.difficuity) {
+      return false;
+    }
+    if (filter.type !== null && item.type !== filter.type) {
+      return false;
+    }
+    if (filter.time !== null && item.time !== filter.time) {
+      return false;
+    }
+    return true;
+  });
+
+  let randomIndex = Math.floor(Math.random() * filterdData.length);
+  let randomObject = filterdData[randomIndex];
+  randomVideo.value = randomObject
+  emit("video",randomObject)
+};
 </script>
-<style lang="">
-  
-</style>
+<style lang=""></style>
