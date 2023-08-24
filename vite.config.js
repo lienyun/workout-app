a0d0cceeca6256169be10fdece05e6d5dcb253be
wendy <lienyun.chang@gmail.com>
2023-08-24 16:00:38 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/workout-app',
 	plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
