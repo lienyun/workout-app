050167131f30608d9695245c171d6c4412724077
wendy <lienyun.chang@gmail.com>
2024-03-06 20:24:55 +0800

    feat: Developer info

--- src/views/Developer.vue
+++ src/views/Developer.vue
@@ -1,14 +1,26 @@
 <template lang="">
   <AppBar></AppBar>
 
-  <v-main>
-    <h3>Wendy Chang</h3>
+  <v-main class="ma-5">
+
+    <div class="myImg"></div>
+    <h3 class="mt-3">Wendy Chang</h3>
   </v-main>
 </template>
+
 <script setup>
 import { ref } from "vue";
 import AppBar from "../components/AppBar.vue";
 
 const drawer = ref(false);
 </script>
-<style lang=""></style>
+
+<style scoped>
+.myImg {
+  width: 300px;
+  height: 300px;
+  background-image: url('/me.JPG');
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
